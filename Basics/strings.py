3c8dcd12bdcd4cfb8edc1d25c9f1fb71d0412695
Devon Long <devonelong@gmail.com>
2022-03-06 20:20:14 -0500

    string methods and formatting

---
+++ Basics/strings.py
@@ -0,0 +1,36 @@
+# myString = "Cisco Router"
+# print_hi(myString)
+# newString = myString.replace(" ", "")
+# print_hi(myString)
+# print_hi(newString)
+#
+#
+# x = "Cisco"
+# y = "Switch"
+# print(x + y)
+# print(x*3)
+# print(x)
+# print("o" in x)
+# print("b" in x)
+# print("b" not in x)
+# print("Cisco model: %s, %d WAN slots, IOS %.1f" % ("2600XM", 2, 12.4))
+# print("Cisco model: {}, {} WAN slots, IOS {}".format("2600XM", 2, 12.4))
+
+# model = '2600XM'
+# slots = 4
+# ios = 12.3
+# print(f"Cisco model: {model}, {slots * 2} WAN slots, IOS {ios}")
+
+# string1 = "Cisco model: 2600XM, 8 WAN slots, IOS 12.4"
+# print(string1[5:12])
+# print(string1[5:])
+# print(string1[:15])
+# print(string1[-5:])
+# print(string1[:-5])
+# print(string1[::2])
+# print(string1[::-1])
+
+
+
+
+
