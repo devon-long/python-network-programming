eee032965a12076298822ba79bc8224eee71951f
Devon Long <devonelong@gmail.com>
2022-03-09 13:45:46 -0500

    custom modules

---
+++ Basics/python_app.py
@@ -0,0 +1,11 @@
+import my_module
+import sys
+
+print("This is python app!")
+
+print(my_module.my_function())
+
+print(my_module.my_var)
+
+
+#python -m pip install <module_name> for non built in modules
